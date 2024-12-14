27013d1e704f8d06986d46d2b4e2a21fdb18fead
MOHAMMAD YUSUF BEEN HASHEM A17KE4015 <yusufbeenhashem335@gmail.com>
2024-12-14 03:06:37 -0700

    Mutation type added.

--- server/src/schema.ts
+++ server/src/schema.ts
@@ -7,6 +7,10 @@ export const typeDefs = gql`
     track(id: ID): Track
   }
 
+  type Mutation {
+    incrementTrackViews(id: ID!): IncrementTrackViewsResponse!
+  }
+
   "A track is a group of Modules that teaches about a specific topic"
   type Track {
     id: ID!
@@ -41,5 +45,16 @@ export const typeDefs = gql`
     id: ID!
     name: String!
     photo: String
+  }
+
+  type IncrementTrackViewsResponse {
+    "Similar to HTTP status code, represents the status of the mutation"
+    code: Int!
+    "Indicates whether the mutation was successful"
+    success: Boolean!
+    "Human-readable message for the UI"
+    message: String!
+    "Newly updated track after a successful mutation"
+    track: Track
   }
 `;
